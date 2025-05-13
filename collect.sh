7fd3708c87aa48144ae203833fd954f0a25e786c
ShawRong <shawnrong1213@gmail.com>
2025-05-13 18:10:25 +0800

    update

--- collect.sh
+++ collect.sh
@@ -4,7 +4,10 @@
 mkdir -p {.config,.config/nvim,.obsidian}  # 按需添加其他分类目录
 
 # 复制配置文件到仓库，保持结构
-cp ~/.zshrc .zshrc
-cp -r ~/.config/nvim .config/nvim
-cp -r ~/Documents/obsidian-library/October/.obsidian .obsidian
+cp ~/.zshrc .
+cp -r ~/.config/nvim .config/
+cp -r ~/.config/karabiner .config/
+cp -r ~/.config/raycast .config/
+cp -r ~/Documents/obsidian-library/October/.obsidian .
+
 
