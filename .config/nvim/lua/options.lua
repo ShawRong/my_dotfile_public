6acb35a9c5605f6a9f4337dc41bd0d313a6ac57b
ShawRong <shawnrong1213@gmail.com>
2025-05-13 16:38:51 +0800

    update

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,3 +1,3 @@
 vim.opt.nu = true -- enable line numbers
 vim.opt.relativenumber = true -- relative line numbers
-vim.opt.clipboard = 'unamedplus' -- system sync copy and pasting
+vim.opt.clipboard = 'unnamedplus' -- system sync copy and pasting
