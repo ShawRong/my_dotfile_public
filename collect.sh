6c2cd8614441cce426531caae9f988955e19abeb
ShawRong <shawnrong1213@gmail.com>
2025-05-13 15:14:17 +0800

    auto collect.

--- collect.sh
+++ collect.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # 确保仓库目录存在
-mkdir -p {.zsh,.config,.config/nvim}  # 按需添加其他分类目录
+mkdir -p {.zsh,.config,.config/nvim,.obsidian}  # 按需添加其他分类目录
 
 # 复制配置文件到仓库，保持结构
 cp ~/.zshrc .zshrc
 cp -r ~/.config/nvim .config/nvim
+cp -r ~/Documents/obsidian-library/October/.obsidian .obsidian
 
 # 显示变更
 git add .
